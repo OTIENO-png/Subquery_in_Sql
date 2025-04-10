6dfe9c6a103dd52009c702643c9a687268c591de
brian <otienolubalo@gmail.com>
2025-04-10 22:32:40 +0300

    Implement subquery to retrieve IT department staff from joined tables

--- subquery.sql
+++ subquery.sql
@@ -1,16 +1,14 @@
-SELECT
-    s.Employee_id_No,
-    s.Department_name,
-    s.Designation,
-    e.Full_Name,
-    e.Email,
-    e.Birth_year,
-    e.Phone_Number,
-    CASE 
-        WHEN s.Employee_id_No IN (SELECT Employee_id_No FROM Employee_Data WHERE Designation IS NULL OR s.Employee_id_No IS NULL OR s.Department_name IS NULL) THEN 'Not onboarded'
-        ELSE 'Fully onboarded'
-    END AS Work_status
-FROM Staffs_Data s
-RIGHT JOIN Employee_Personal_Info e
-ON s.Employee_id_No = e.Employee_id_No
-ORDER BY s.Employee_id_No DESC;
+SELECT  
+s.Employee_id_No,  
+s.Department_name,  
+s.Designation,
+e.Full_Name ,
+e.Email,  
+e.DOB,  
+e.Phone_Number
+FROM  
+    (SELECT * FROM Staffs_Data WHERE Department_name = 'IT') AS s  
+INNER JOIN  
+    Employee_Personal_Info e  
+ON  
+    s.Employee_id_No = e.Employee_id_No;
